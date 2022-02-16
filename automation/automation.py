ccff93e35b046c73a6f3c44dbc3ae668bec69063
giochoa <giochoa0422@gmail.com>
2022-02-16 13:43:09 +0900

    last for this section

--- automation/automation.py
+++ automation/automation.py
@@ -8,6 +8,8 @@ chrome_browser.maximize_window()
 chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
 
 show_message_button = chrome_browser.find_element_by_class_name("btn-default")
+#underneath is the selector using the css to select all the children button of the specified id
+# user_button2 = chrome_browser.find_element_by_css_selector('#get-input > .btn')
 
 print(show_message_button.get_attribute("innerHTML"))
 
@@ -17,3 +19,7 @@ print(show_message_button.get_attribute("innerHTML"))
 chrome_browser.find_element_by_id('user-message').send_keys('初めまして')
 
 show_message_button.click()
+
+chrome_browser.close()
+# chrome_browser.close()
+# chrome_browser.quit()
